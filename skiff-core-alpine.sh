07b83ea6d6dbd7af30f56df94ac76df9c32cefcc
Christian Stewart <christian@aperture.us>
2024-07-01 17:01:22 -0700

    feat: add skiff-core-alpine
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ skiff-core-alpine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export IMAGE_NAME="skiff-core-alpine"
+export IMAGE_DIR="./skiffos/configs/core/alpine/buildroot_ext/package/skiff-core-alpine/coreenv"
+export IMAGE_TAG="quay.io/skiffos/skiff-core-alpine:latest"
+
+source common.sh
