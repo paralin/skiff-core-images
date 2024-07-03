35f15fa888d5fd256ff335c0042e929cd7b9a264
Christian Stewart <christian@aperture.us>
2024-07-02 22:25:58 -0700

    common: use not-buildx on default
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- common.sh
+++ common.sh
@@ -10,42 +10,42 @@ echo "Building cross-platform $IMAGE_NAME image..."
 cd $IMAGE_DIR
 
 # note: buildx currently not working with quay.io correctly
-# export USE_DOCKER_BUILD=true to use docker build instead
+# export USE_BUILDX=true to use buildx instead
 
-if [ -z "$USE_DOCKER_BUILD" ]; then
+if [ -z "${ARCH_TAGS[*]}" ]; then
+    ARCH_TAGS=(
+        arm
+        arm64
+        amd64
+        riscv64
+    )
+fi
+ARCH_IMAGES=("${ARCH_TAGS[@]/#/${IMAGE_NAME}:}")
+
+tag_to_platform() {
+	case $1 in
+	"arm") echo "linux/arm/v7" ;;
+	"arm64") echo "linux/arm64" ;;
+	"amd64") echo "linux/amd64" ;;
+	"riscv64") echo "linux/riscv64" ;;
+	*) echo "unknown" ;;
+	esac
+}
+
+if [ -n "$USE_BUILDX" ]; then
 	# Use buildx
 	echo "Using buildx to build and push the image..."
+	platforms=$(printf "linux/%s," "${ARCH_TAGS[@]}" | sed 's/arm/arm\/v7/' | sed 's/,$//')
 	docker buildx build \
 		--push \
 		--tag ${IMAGE_NAME}:${IMAGE_TAG} \
 		--output=type=image,push=true,oci-mediatypes=false \
-		--platform linux/amd64,linux/arm,linux/arm64,linux/riscv64 \
+		--platform $platforms \
 		-f Dockerfile \
 		.
 else
 	# Use the multi-arch approach
 	echo "Using the multi-arch approach to build and push the image..."
-
-	ARCH_TAGS=(
-		arm
-		arm64
-		amd64
-		riscv64
-		i386
-	)
-	ARCH_IMAGES=("${ARCH_TAGS[@]/#/${IMAGE_NAME}:}")
-
-	tag_to_platform() {
-		case $1 in
-		"arm") echo "linux/arm/v7" ;;
-		"arm64") echo "linux/arm64" ;;
-		"amd64") echo "linux/amd64" ;;
-		"riscv64") echo "linux/riscv64" ;;
-		"i386") echo "linux/386" ;;
-		*) echo "unknown" ;;
-		esac
-	}
-
 	# Build and push for each architecture
 	for tag in "${ARCH_TAGS[@]}"; do
 		platform=$(tag_to_platform $tag)
