718dab9b4e5f59510b38445c41015d909294336f
Christian Stewart <christian@aperture.us>
2024-07-02 19:10:38 -0700

    add skiff-core-nixos
    Signed-off-by: Christian Stewart <christian@aperture.us>

---
+++ skiff-core-nixos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export IMAGE_NAME="quay.io/skiffos/skiff-core-nixos"
+export IMAGE_DIR="./skiffos/configs/core/nixos/buildroot_ext/package/skiff-core-nixos/coreenv"
+export IMAGE_TAG="latest"
+
+source common.sh
