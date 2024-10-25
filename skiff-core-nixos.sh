2f97a8996241a32a8913b4e1ee3e06ed47c16518
Christian Stewart <christian@aperture.us>
2024-10-25 13:18:42 -0700

    WIP
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- skiff-core-nixos.sh
+++ skiff-core-nixos.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 export IMAGE_NAME="quay.io/skiffos/skiff-core-nixos"
-export IMAGE_DIR="./skiffos/configs/core/nixos/buildroot_ext/package/skiff-core-nixos/coreenv"
+export IMAGE_DIR="./docker-nixos"
 export IMAGE_TAG="latest"
+
+# NixOS only supports arm64 and amd64
+ARCH_TAGS=(
+    arm64
+    amd64
+)
 
 source common.sh
